a4aad33081fa44f8f05835ea5799ccd26edf3a5e
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-02-23 04:00:55 +0530

    render prod test

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/fake-api',
+  apiUrl: 'https://spring-boot-security-angular-client-csrf.onrender.com',
+  //apiUrl: 'http://localhost:4000/fake-api',
   //apiUrl: 'http://localhost:8080',
 };
