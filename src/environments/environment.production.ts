433d2c288990f3b76330af97b22010b6ef1a0912
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-02-23 10:54:30 +0530

    change prod url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://spring-boot-security-angular-client-csrf.onrender.com',
+  apiUrl: 'https://spring-boot-security-ren-prod-angular.onrender.com',
   //apiUrl: 'http://localhost:4000/fake-api',
   //apiUrl: 'http://localhost:8080',
 };
